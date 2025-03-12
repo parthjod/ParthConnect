234aaedb1e73e77331ccbe1fcbb8803c8a6fc268
parthjod <hlomello765@gmail.com>
2025-03-12 14:13:33 +0530

    updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,13 +13,7 @@ const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", process.env.PORT || 3000);
-app.use(
-  cors({
-    origin: "http://localhost:3001",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
+app.use(cors({}));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
